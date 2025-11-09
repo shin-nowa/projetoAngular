2b130f4d9bfbe5e5584be90e5552d5db2f7e652d
shin-nowa <kkeusoeu@gmail.com>
2025-11-09 18:35:31 -0300

    Adição da Pipe Personalizada

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,25 +1,25 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+// import { Injectable, inject } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { environment } from '../../environments/environment';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ApiService {
-  private base = environment.apiUrl;
-  private http = inject(HttpClient);
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ApiService {
+//   private base = environment.apiUrl;
+//   private http = inject(HttpClient);
 
-  getHello(): Observable<any> {
-    return this.http.get(`${this.base}/hello`);
-  }
+//   getHello(): Observable<any> {
+//     return this.http.get(`${this.base}/hello`);
+//   }
 
-  get<T>(path: string): Observable<T> {
-    return this.http.get<T>(`${this.base}${path}`);
-  }
+//   get<T>(path: string): Observable<T> {
+//     return this.http.get<T>(`${this.base}${path}`);
+//   }
 
-  post<T>(path: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.base}${path}`, body);
-  }
-}
+//   post<T>(path: string, body: any): Observable<T> {
+//     return this.http.post<T>(`${this.base}${path}`, body);
+//   }
+// }
        
